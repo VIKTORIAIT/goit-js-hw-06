c9fce6cb3c0f1e9628d381d4643e748ce36000c4
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2021-11-08 00:50:39 +0200

    add 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,5 +2,32 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+let defAmount = 0;
+const elControls = document.querySelector("#controls");
 const input = document.querySelector("input");
-const btn = dc;
+const btnCreate = document.querySelectorAll("button")[0];
+const btnDestroy = document.querySelectorAll("button")[1];
+const form = document.querySelector("#boxes");
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(event) {
+  let amount = input.value;
+  const boxes = [];
+  if (amount > 0) {
+    for (let i = 0; i < amount; i += 1) {
+      const divEl = document.createElement("div");
+      divEl.style.width = 30 + 10 * (defAmount + i) + "px";
+      divEl.style.height = 30 + 10 * (defAmount + i) + "px";
+      divEl.style.backgroundColor = getRandomHexColor();
+      boxes.push(divEl);
+    }
+  }
+  defAmount += Number(amount);
+  form.append(...boxes);
+}
+
+function destroyBoxes() {
+  form.innerHTML = "";
+  defAmount = 0;
+}
